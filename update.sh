8abd5ad62c1ee08e41b8f26fd6594e8ceb490e0d
ch4xer <ch4xer@gmail.com>
2022-05-24 18:11:54 +0800

    update

---
+++ update.sh
@@ -0,0 +1,10 @@
+cp -r ~/.config/alacritty ./alacritty
+cp -r ~/.config/ranger ./ranger
+cp -r ~/.config/sway ./sway
+cp -r ~/.config/swaylock ./swaylock
+cp -r ~/.config/waybar ./waybar
+cp -r ~/.config/zathura ./zathura
+cp -r ~/.config/rofi ./rofi
+cp -r ~/.zshrc ./zsh/zshrc
+
+

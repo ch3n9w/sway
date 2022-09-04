ac76adac0239aa5d4c4626d40e938c39c5ff4050
ch4xer <ch4xer@gmail.com>
2022-09-04 20:36:06 +0800

    add ranger config

--- ranger/fzf-scope.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-case "$1" in
-	*.md) glow -s dark "$1" ;;
-	*.zip) zipinfo "$1" ;;
-	*.7z) 7z l "$1" ;;
-	*) bat --color=always --italic-text=always --style=numbers,changes,header --line-range :500 "$1" ;;
-esac

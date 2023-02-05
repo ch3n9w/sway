1e8fd9bfbb26f7a4a458b388a15dcd997f7b1f97
ch3n9w <ch3n9w@gmail.com>
2023-02-05 21:57:01 +0800

    set globalstatus for lualine

--- nvim/lua/plugins/config/lualine.lua
+++ nvim/lua/plugins/config/lualine.lua
@@ -1,23 +1,26 @@
-M = function ()
+M = function()
     require('lualine').setup {
+        options = {
+            globalstatus = true,
+        },
         sections = {
             lualine_a = {
                 {
                     'fileformat',
                     symbols = {
                         unix = '', -- e712
-                        dos = '',  -- e70f
-                        mac = '',  -- e711
+                        dos = '', -- e70f
+                        mac = '', -- e711
                     }
                 },
                 'mode'
             },
             -- lualine_b = {'branch', 'diff', 'diagnostics'},
-            lualine_b = {'diff', 'diagnostics'},
-            lualine_c = {'filename'},
-            lualine_x = {'encoding','filetype'},
-            lualine_y = {'progress'},
-            lualine_z = {'location'}
+            lualine_b = { 'diff', 'diagnostics' },
+            lualine_c = { 'filename' },
+            lualine_x = { 'encoding', 'filetype' },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' }
         }
     }
 end
